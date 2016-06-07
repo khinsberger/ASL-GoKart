d4eec4067ec3a9e4e23b93c19fd49c6469974914
khinsberger <k.hinsberger@googlemail.com>
2016-06-07 20:06:17 +0200

    Warnleuchten + Bug Fixes
    Neues Konzept zur Ansteuerung der Warnleuchten, Bug Fixes bzgl. des
    Temperaturreglers und der Ventilatorgeschwindigkeit

--- Regelung_BZSystem/Regelung_BZSystem.ino
+++ Regelung_BZSystem/Regelung_BZSystem.ino
@@ -22,18 +22,10 @@
  * Pin 8: Wasserpumpe
  * 
  * -- User Interface --
- * Pin 5: Warnleuchte Temperatur zu hoch (TH)
- * Pin 6: Warnleuchte Temperatur zu niedrig (TN)
- * Pin 7: Warnleuchte kein Durchfluss (D)
+ * Pin 30: Warnleuchte Temperatur zu hoch (TH)
+ * Pin 31: Warnleuchte Temperatur zu niedrig (TN)
+ * Pin 32: Warnleuchte kein Durchfluss (D)
  * 
- * Created 8th December 2015
- * By AerospaceLab Herrenberg
- * Modified 15th December 2015
- * By AerospaceLab Herrenberg
- * Modified 22nd December 2015
- * By AerospaceLab Herrenberg
- * Modified 19th Janurary 2016
- * By Aerospacelab Herrenberg
  */
 
 /*
@@ -60,9 +52,9 @@ const int ventilatorPin         = 3;
 const int kompressorPin         = 4;
 const int wasserpumpePin        = 8;
 // User Interface
-const int warnleuchtenPin_TH    = 5;
-const int warnleuchtenPin_TN    = 6;
-const int warnleuchtenPin_D     = 7;
+const int warnleuchtenPin_TH    = 30;
+const int warnleuchtenPin_TN    = 31;
+const int warnleuchtenPin_D     = 32;
 
 
 /*
@@ -71,13 +63,13 @@ const int warnleuchtenPin_D     = 7;
 double temperatur = 0;
 double durchfluss = 0;
 volatile int nbTopsFan;
-int Ventilator_vel;
+int Ventilator_vel = 100;
 
 /*
  * Initialisieren der globalen Parameter
  */
 const int R2 = 10000;
-const double sollTemp = 21;
+const double sollTemp = 28.8;
 
 
 
@@ -109,14 +101,12 @@ void setup() {
   Serial.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
   Serial.println("Serielle Kommunikation initialisiert");
   Serial.println("Initiale Daten:");
-
   getTemperatur();
   Serial.print("Temperatur: ");
   Serial.print(temperatur);
   Serial.println(" deg C");
-  setWasserpumpe(255);
-  setVentilator(255);
   while(durchfluss <= 0.1) {
+    setWarnleuchten(3,1);
     nbTopsFan = 0;
     sei();    // Aktivieren
     delay(500);
@@ -132,6 +122,7 @@ void setup() {
       break;
     }
   }
+  setWarnleuchten(3,0);
   Serial.println("System bereit, Start der Regelung");
 }
 
@@ -153,8 +144,10 @@ void loop() {  //Durchflusskontrolle und Kompensierung
       setVentile(2, 0);
       setVentile(3, 0);
       setKompressor(0);               //Pumpen aus außer Wasserpumpe
-      setWarnleuchten(3); 
+      setWarnleuchten(3,1);
     }
+  } else {
+    setWarnleuchten(3,0);
   }
 
         Serial.print("Durchfluss: ");
@@ -168,17 +161,25 @@ void loop() {  //Durchflusskontrolle und Kompensierung
   
   getTemperatur();
   double diff = temperatur - sollTemp;
-  double reg = diff / 5.0;
+  double reg = diff / 0.5;
   Ventilator_vel = Ventilator_vel * ( 1 + reg);
+  Ventilator_vel = constrain(Ventilator_vel,100,255);
   Serial.print("Ventilator: ");
   Serial.println(Ventilator_vel);
   setVentilator(Ventilator_vel);
-  if(temperatur > 75) {
+  if(temperatur > 30) {
     setVentile(1, 0);            //alle Ventile aus
     setVentile(2, 0);
     setVentile(3, 0);
     setKompressor(0);            //Pumpen aus außer Wasserpumpe
-    setWarnleuchten(3);
+    setWarnleuchten(2,1);
+    setWarnleuchten(1,0);
+  } else if (temperatur < 29) {
+    setWarnleuchten(1,1);
+    setWarnleuchten(2,0);
+  } else {
+    setWarnleuchten(1,0);
+    setWarnleuchten(2,0);
   }
     
   if(notAusPin == 1) {
@@ -186,7 +187,7 @@ void loop() {  //Durchflusskontrolle und Kompensierung
     setVentile(2, 0);
     setVentile(3, 0);
     setKompressor(0);            //Pumpen aus außer Wasserpumpe
-    setWarnleuchten(3);          // Warnleuchte
+    setWarnleuchten(3,1);          // Warnleuchte
   }
 }
 
@@ -197,7 +198,7 @@ void getTemperatur() {
   double value = analogRead(temperaturPin);
   double volt = map(value, 0, 1023, 0, 5000);
   double widerstand = (R2/(volt/5000))-R2;
-  temperatur=92.439-27.929*log(widerstand/1000);
+  temperatur=92.1936-28.3997*log(widerstand/1000);
 }
 
 /*
@@ -268,14 +269,26 @@ void setKompressor(int vel) {
  * INPUT:
  * nbr (int): Nummer der zu ansteuernden LED
  */
-void setWarnleuchten(int nbr) {
-  if(nbr == 1) {
-    digitalWrite(warnleuchtenPin_TH, HIGH);
+void setWarnleuchten(int nbr, int state) {
+  // Warnleuchte TH nbr=1
+  if(nbr == 1 && state == 0) {
+      analogWrite(warnleuchtenPin_TH, 0);
   }
-  if(nbr == 2) {
+  else if(nbr == 1 && state == 1) {
+    analogWrite(warnleuchtenPin_TH, 255);
+  }
+  // Warnleuchte TN nbr=2
+  if(nbr == 2 && state == 0) {
+    digitalWrite(warnleuchtenPin_TN, LOW);
+  }
+  else if(nbr == 2 && state == 1){
     digitalWrite(warnleuchtenPin_TN, HIGH);
   }
-  if(nbr == 3) {
+  // Warnleuchte D nbr=3
+  if(nbr == 3 && state == 0) {
+    digitalWrite(warnleuchtenPin_D, LOW);
+  }
+  else if(nbr == 3 && state == 1) {
     digitalWrite(warnleuchtenPin_D, HIGH);
   }
 }
